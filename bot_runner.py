95cd7f7fd39af84f1efd6d3b7ffc76f27ebdd680
yokostan <>
2020-10-18 15:41:58 -0700

    Update bot_runner.py
    Add reactions into y(feature)s in train_test_split()

--- bot_runner.py
+++ bot_runner.py
@@ -22,33 +22,46 @@ def read_csv_to_df():
             val_ratio = float(line.split('=',1)[1])
     return post_df, comment_df, train_ratio, test_ratio, val_ratio
 
-def train_test_val_split(X, y, train_ratio, test_ratio, val_ratio=0):
+def train_test_val_split(df, type, train_ratio, test_ratio, val_ratio=0):
     # train test ratio should sum up to 1 if val ratio is not provided
     # otherwise, the three ratios should sum up to 1
     if train_ratio + test_ratio + val_ratio - 1 > 1e-5:
         print("please provide eligible train, test, val ratio")
-        return None, None, None, None, None, None
+        return [[]*24]
     else:
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_ratio, random_state=1)
-        if val_ratio != 0:
-            X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=val_ratio/(train_ratio+val_ratio), random_state=1) # test_size = second split ratio between train and val
-        return X_train, y_train, X_test, y_test, X_val, y_val
+        if type == 'post':
+            X = df['message'].to_list()
+            y0 = df['label'].to_list()
+            y1 = df['react_angry'].to_list()
+            y2 = df['react_haha'].to_list()
+            y3 = df['react_like'].to_list()
+            y4 = df['react_love'].to_list()
+            y5 = df['react_sad'].to_list()
+            y6 = df['react_wow'].to_list()
+            X_train, X_test, y0_train, y0_test, y1_train, y1_test, y2_train, y2_test, y3_train, y3_test, \
+            y4_train, y4_test, y5_train, y5_test, y6_train, y6_test = train_test_split(X, y0, y1, y2, y3, y4, y5, y6, test_size=test_ratio, random_state=1)
+            if val_ratio != 0:
+                X_train, X_val, y0_train, y0_val, y1_train, y1_val, y2_train, y2_val, y3_train, y3_val, \
+                y4_train, y4_val, y5_train, y5_val, y6_train, y6_val = train_test_split(X_train, y0_train, y1_train, y2_train, y3_train, \
+                    y4_train, y5_train, y6_train, test_size=val_ratio/(train_ratio+val_ratio), random_state=1) # test_size = second split ratio between train and val
+            return [X_train, X_test, y0_train, y0_test, y1_train, y1_test, y2_train, y2_test, y3_train, y3_test, y4_train, y4_test, y5_train, y5_test, \
+            y6_train, y6_test, X_val, y0_val, y1_val, y2_val, y3_val, y4_val, y5_val, y6_val]
 
 def main():
     post_df, comment_df, train_ratio, test_ratio, val_ratio = read_csv_to_df()
     # problem 1: sentiment analysis
     # reminder: the data split is different each time we run the program
     #           so the models are only comparable from the same run
-    post_X_train, post_y_train, post_X_test, post_y_test, post_X_val, post_y_val = train_test_val_split(post_df['message'].tolist(), post_df['label'].tolist(), train_ratio, test_ratio, val_ratio)
-    if all(v is not None for v in [post_X_train, post_y_train, post_X_test, post_y_test]):
+    post_data_list = train_test_val_split(post_df, 'post', train_ratio, test_ratio, val_ratio)
+    if post_data_list is not None:
         # method 1: sgd model
         sgd = StochasticGradientDescent()
         # baseline: using default values for parameters
-        model = sgd.train(post_X_train, post_y_train, post_X_val, post_y_val)
-        sgd.predict(model, post_X_test, post_y_test, 'sgd_baseline')
+        model = sgd.train(post_data_list[0], post_data_list[2], post_data_list[16], post_data_list[17])
+        sgd.predict(model, post_data_list[1], post_data_list[3], 'sgd_baseline')
         # sgd model with tuned paramters
-        model = sgd.train(post_X_train, post_y_train, post_X_val, post_y_val, losses=['hinge', 'log', 'squared_loss'], learning_rates=['optimal'], tols=[1e-5, 1e-4, 1e-3], max_iter=500)
-        sgd.predict(model, post_X_test, post_y_test, 'sgd_tuned')
+        model = sgd.train(post_data_list[0], post_data_list[2], post_data_list[16], post_data_list[17], losses=['hinge', 'log', 'squared_loss'], learning_rates=['optimal'], tols=[1e-5, 1e-4, 1e-3], max_iter=500)
+        sgd.predict(model, post_data_list[1], post_data_list[3], 'sgd_tuned')
 
 if __name__ == '__main__':
    main()

51d42ddf4b69231338a54a6f7bc5b5ef486ae3e6
yokostan <>
2020-10-18 21:14:04 -0700

    Update sentiment_analysis.py
    add sentiment conversion

--- model/sentiment_analysis.py
+++ model/sentiment_analysis.py
@@ -94,8 +94,9 @@ class KMeansModel():
                         max_count = value[1]
                         max_cluster_index = value[0]
                 cluster_index_map[key] = max_cluster_index  
-                
-            return [cluster_index_map.get(y_pred[i]) for i in range(len(y_pred))]
+
+            mapped_y_pred = [cluster_index_map.get(y_pred[i]) for i in range(len(y_pred))]
+            return mapped_y_pred
 
     def get_accuracy(self, y_true, y_pred):
         count = 0
@@ -104,47 +105,70 @@ class KMeansModel():
                 count += 1
         return count/len(y_true)
 
-    def train(self, X_train, X_val, y_val_list, n_clusters_list=None, tols=None, max_iter=None):
+    def get_sentiment(self, y):
+        sentiment = [0]*len(y)
+        for i in range(len(y)):
+            if y[i] == 2 or y[i] == 3:
+                sentiment[i] = 1
+            elif y[i] == 0 or y[i] == 4:
+                sentiment[i] == -1
+            else:
+                sentiment[i] == 0
+        return sentiment
+
+    def train(self, X_train, X_val, y_val_list, n_clusters_list=None, tols=None, max_iters=None):
         def get_param(param, default):
             return param if param is not None else default  
 
         X_train = self.count_vect.fit_transform(['' if x is np.nan else x for x in X_train]) 
         y_val_category = self.categorize_reaction(y_val_list)
         if len(X_val) > 0: # we have val data to tune the params
-            max_accuracy = -1e5
+            max_accuracy = 0
             current_best_model = None
             index = 0
             n_clusters_list = get_param(n_clusters_list, [6])
             tols = get_param(tols,[1e-3])
+            max_iters = get_param(max_iters,[300])
             for n_clusters in n_clusters_list:
                 for tol in tols:
-                    print("Number "+str(index)+" training started")
-                    model = KMeans(n_clusters=n_clusters, max_iter=get_param(max_iter,300), tol=tol)
-                    model.fit(X_train)
-                    y_val_pred = model.predict(self.count_vect.transform(['' if x is np.nan else x for x in X_val]))
-                    y_val_pred = self.clusters_mapping(y_val_category, y_val_pred)
-                    accuracy = self.get_accuracy(y_val_category, y_val_pred)
-                    if accuracy > max_accuracy:
-                        current_best_model = model
-                        max_accuracy = accuracy
-                    index += 1             
+                    for max_iter in max_iters:
+                        print("Number "+str(index)+" training started")
+                        model = KMeans(n_clusters=n_clusters, max_iter=max_iter, tol=tol, random_state=10)
+                        model.fit(X_train)
+                        y_val_pred = model.predict(self.count_vect.transform(['' if x is np.nan else x for x in X_val]))
+                        y_val_pred = self.clusters_mapping(y_val_category, y_val_pred)
+                        y_val_sentiment = self.get_sentiment(y_val_category)
+                        y_val_pred_sentiment = self.get_sentiment(y_val_pred)
+                        accuracy = self.get_accuracy(y_val_sentiment, y_val_pred_sentiment)
+                        if accuracy > max_accuracy:
+                            current_best_model = model
+                            max_accuracy = accuracy
+                            cluster_accuracy = self.get_accuracy(y_val_category, y_val_pred)
+                        index += 1             
         else:
             print("training started")
-            current_best_model = KMeans(n_clusters=6, max_iter=300, tol=1e-4)
+            current_best_model = KMeans(n_clusters=6, max_iter=300, tol=1e-4, random_state=10)
             current_best_model.fit(X_train)
         print('The parameters of the best model are: ')
         print(current_best_model)
         if len(X_val) > 0:
-            print('The validation accuracy is '+str(max_accuracy))
+            print('The validation accuracy of sentiment is '+str(max_accuracy))
+            print('The validation accuracy of cluster for the model with highest sentiment accuracy is '+str(cluster_accuracy))
         return current_best_model
 
     def predict(self, model, X_test, y_test, y_test_list, filename):
         y_test_category = self.categorize_reaction(y_test_list)
-        y_pred = model.predict(self.count_vect.transform(['' if x is np.nan else x for x in X_test]))
-        y_pred = self.clusters_mapping(y_test_category, y_pred)
+        y_test_pred = model.predict(self.count_vect.transform(['' if x is np.nan else x for x in X_test]))
+        y_test_pred = self.clusters_mapping(y_test_category, y_test_pred)
+        y_test_sentiment = self.get_sentiment(y_test_category)
+        y_test_pred_sentiment = self.get_sentiment(y_test_pred)
+        sentiment_accuracy = self.get_accuracy(y_test_sentiment, y_test_pred_sentiment)
+        cluster_accuracy = self.get_accuracy(y_test_category, y_test_pred)
         df = pd.DataFrame ({'message': X_test,'true_react_angry': y_test[0], 'true_react_haha': y_test[1], 'true_react_like': y_test[2], 'true_react_love': y_test[3], 'true_react_sad': y_test[4], 'true_react_wow': y_test[5], \
-            'true_cluster_id': y_test_category, 'pred_cluster_id': y_pred})
+            'true_cluster_id': y_test_category, 'pred_cluster_id': y_test_pred, \
+            'true_sentiment': y_test_sentiment, 'pred_sentiment': y_test_pred_sentiment})
         df.to_csv(filename+'.csv', index=False)  
-        accuracy = self.get_accuracy(y_test_category, y_pred)
-        print('The prediction accuracy is '+str(accuracy))
+        
+        print('The prediction accuracy of clusters is '+str(sentiment_accuracy))
+        print('The prediction accuracy of sentiment is '+str(cluster_accuracy))
         print('---------------------------------------------------------------------------')

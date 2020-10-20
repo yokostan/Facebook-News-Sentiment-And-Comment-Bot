b39031e2595c895efaf44b5671e23d84cb1a35fc
yokostan <>
2020-10-19 17:25:25 -0700

    Update bot_runner.py
    Comment generation data preprocessing

--- bot_runner.py
+++ bot_runner.py
@@ -2,6 +2,8 @@
 # the bot_runner is composed of two part
 #   part 1: sentiment analysis
 #   part 2: comment generation
+import csv
+import difflib
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
@@ -77,12 +79,64 @@ def train_test_val_split(df, type, train_ratio, test_ratio, val_ratio=0):
                     y4_train, y5_train, y6_train, test_size=val_ratio/(train_ratio+val_ratio), random_state=1) # test_size = second split ratio between train and val
             return [X_train, X_val, y0_train, y0_val, y1_train, y2_train, y3_train, y4_train, y5_train, y6_train, \
                 y1_val, y2_val, y3_val, y4_val, y5_val, y6_val, X_test, y0_test, y1_test, y2_test, y3_test, y4_test, y5_test, y6_test]
+        else:
+            X = [row[0] for row in df]
+            y = [row[1] for row in df]
+            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_ratio, random_state=1)
+            X_val = y_val = None
+            if val_ratio != 0:
+                X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=val_ratio/(train_ratio+val_ratio), random_state=1) # test_size = second split ratio between train and val
+            return [X_train, X_val, X_test, y_train, y_val, y_test]
+
+def generate_post_comment_nested_dict(post, comment, n):
+    def ngrams(comment, n):
+        output = []
+        if isinstance(comment, str):
+            comment = comment.split(' ')
+            for i in range(len(comment)-n+1):
+                output.append(comment[i:i+n])
+        return output
+
+    # merge post and comment df and only take the messsage and comment columns 
+    comment = comment.rename(columns={"message": "comment", "post_id": "post_id_1"})
+    new_df = pd.merge(post, comment, left_on='post_id', right_on='post_id_1', how='left')[['message', 'comment']]
+    
+    # perform n gram to generate word pairs for words in comments
+    post_comment_dict = {}
+    for index, row in new_df.iterrows():
+        post = row['message']
+        ngram_list = ngrams(row['comment'], 2)
+        # construct a nested dict where the key is post 
+        # and the value is a word dict for n-gram words from its comments
+        word_dict = post_comment_dict.get(post, {})
+        for word_1, word_2 in ngram_list:
+            if word_1 in word_dict.keys():
+                word_dict[word_1].append(word_2)
+            else:
+                word_dict[word_1] = [word_2]
+        post_comment_dict[post] = word_dict
+    with open('post_comment_dict.csv', 'w') as f:
+        for key in  post_comment_dict.keys():
+            f.write("%s,%s\n"%(key, post_comment_dict[key]))
+    return post_comment_dict
+
+def transform_dict_to_list(data_dict):
+    data_list = []
+    for key in data_dict:
+        word_pairs = []
+        word_dict = data_dict.get(key)
+        for word_1 in word_dict:
+            for word_2 in word_dict.get(word_1):
+                word_pair = (' ').join([word_1, word_2]) #word_pair = "word1 word2"
+                word_pairs.append(word_pair)
+        data_list.append([key, word_pairs])
+    return data_list
 
 def main():
     post_df, comment_df, train_ratio, test_ratio, val_ratio = read_csv_to_df()
     # switch for two problems
-    sentiment_analysis = True
-    comment_generation = False
+    sentiment_analysis = False
+    comment_generation = True
     # problem 1: sentiment analysis
     # reminder: the data split is different each time we run the program
     #           so the models are only comparable from the same run
@@ -115,5 +169,12 @@ def main():
         svm = SVMModel()
         model = svm.train(X_train, y0_train, X_val, y0_val, kernels=['linear', 'poly', 'rbf', 'sigmoid'], gammas=['auto', 'scale'], tols=[1e-5, 1e-4, 1e-3], max_iters=[300, 500])
         svm.predict(model, X_test, y0_test, post_data_list[18:], 'svm')
+
+    # problem 2: comment generation
+    # comment_df = remove_null_rows(comment_df) # removed because this takes time and there is no null row in comment
+    post_comment_dict = generate_post_comment_nested_dict(post_df, comment_df, 2)
+    post_comment_dict_list = transform_dict_to_list(post_comment_dict)
+    X_train, X_val, X_test, y_train, y_val, y_test = train_test_val_split(post_comment_dict_list, 'comment', train_ratio, test_ratio, val_ratio)
+
 if __name__ == '__main__':
    main()
